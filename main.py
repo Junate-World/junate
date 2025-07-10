d49bffda0e8591a3cb0d55aab049f7a4af1914f0
Junate-World <abel.ogbonna26@gmail.com>
2025-07-10 13:43:07 +0100

    signup password hasher

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
